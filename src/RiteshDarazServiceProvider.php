0f3433b8deb41cc3e37ffe4e0fafdba63479c212
Ritesh Ghimire <riteshghimire9090@gmail.com>
2020-07-25 20:35:58 +0545

    first commit

---
+++ src/RiteshDarazServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ritesh\Daraz;
+use Illuminate\Support\ServiceProvider;
+class RiteshDarazServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+     public function register()
+    {
+        
+    }
+}
